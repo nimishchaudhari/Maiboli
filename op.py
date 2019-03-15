dfe7c0d5bed4704074d396a38e050a5a60de59e5
nimishchaudhari <nimish.mailbox@gmail.com>
2019-03-16 00:15:30 +0530

    V2 has working dictionary with all thereabouts working.

---
+++ op.py
@@ -0,0 +1,10 @@
+print("string")
+
+print("string")
+
+print("string")
+x=3
+if(x<=3):
+	print("if")
+else:
+	print("else")
--- v2.py
+++ v2.py
@@ -1,7 +1,13 @@
-#Version 2 - using a dictionary json file to replace the phrases
+#Version 2 - using a dictionary list python file to replace the phrases
 
 import os,sys,json
 import dix
+#import tkinter as tk
+
+
+# root = tk.Tk()
+# frame = tk.Frame(root)
+# frame.pack()
 
 #input filename in command line
 arg1 = sys.argv[1]
@@ -12,21 +18,32 @@ eng=open('op.py','w')
 
 #open the input file as read
 file = open(arg1,'r')
+#file = open('Test.py','r')
 
 total_dictionary_items = len(dix.en)
 #Read the input file
-arr = file.readlines()
+arr = file.readlines() # Grabbing the IP file's lines and putting them all together as an array item per line
 i = len(arr)
+
+for y in arr:
+        for x in dix.en:
+                count = dix.en.index(x)
+                y = y.replace(dix.mar[count],dix.en[count])
+        eng.write(y)
+"""
 for y in arr:
     for x in dix.en:
         count = dix.en.index(x)
-        y = y.replace(dix.mar[count],dix.en[count])
-        #yield(x)
-        
-    eng.write(y)
+#Here, I'm supposed to add a logic to exclude the ddictionary translation of open and close of quote marks,
+#This is to avoid bloody replacing of words from inside statements.
+        y = y.replace(dix.mar[count],dix.en[count])        
+
+"""
 file.close()
 eng.close()
-eng = open('op.py','r')
-ip = eng.read()
-exec(ip)
-os.remove('op.py')
+def final_run():
+        eng = open('op.py','r')
+        ip = eng.read()
+        exec(ip)
+        #os.remove('op.py')
+final_run()
