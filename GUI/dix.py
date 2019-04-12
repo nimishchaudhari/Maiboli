c813e2c43149e095f4807bace637b594c96e201b
nimishchaudhari <nimish.chaudhari216@siesgst.ac.in>
2019-04-12 13:14:56 +0530

    optimizing started

--- GUI/dix.py
+++
@@ -1,3 +0,0 @@
-#Python file that has two lists that work as the dictionary
-en = ['print','for','if','elif','else','return','input','exec','float','int','len','max','min','range','round','str','sum','format']
-mar = ['छापा','तोपर्यंत','जर','किंवा','नाहीतर','परत','माहिती','चालवा','दशांश','पूर्णांक','लांबी','कमाल','किमान','रांग','पूर्ण','ओळ','बेरीज','स्वरूप']
