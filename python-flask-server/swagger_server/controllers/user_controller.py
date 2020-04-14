bbfef0acb9c88b3bc3144dbe28fabd416e050743
Nimish Chaudhari <nimishchaudhari@MacBook-Air.local>
2020-04-14 18:14:30 +0200

    for elijah to see

--- python-flask-server/swagger_server/controllers/user_controller.py
+++ python-flask-server/swagger_server/controllers/user_controller.py
@@ -1,8 +1,23 @@
 import connexion
 import six
-
+import pyrebase
 from swagger_server.models.user import User  # noqa: E501
 from swagger_server import util
+
+firebaseConfig = {
+  "apiKey": "AIzaSyDNtFUjYR4pp09Vx526eDrcX4jjIvq-1XM",
+  "authDomain": "maiboli-rest-api.firebaseapp.com",
+  "databaseURL": "https://maiboli-rest-api.firebaseio.com",
+  "projectId": "maiboli-rest-api",
+  "storageBucket": "maiboli-rest-api.appspot.com",
+  "messagingSenderId": "785641769787",
+  "appId": "1:785641769787:web:1818d34874193d52ee051e",
+  "measurementId": "G-WD78BEMMP3"
+}
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
+#users = db.child("user/test_user/").get()
+
 
 
 def create_user(body):  # noqa: E501
@@ -17,7 +32,11 @@ def create_user(body):  # noqa: E501
     """
     if connexion.request.is_json:
         body = User.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    users = db.child("user").get()
+    print(users.val()) # {"Morty": {"name": "Mortimer 'Morty' Smith"}, "Rick": {"name": "Rick Sanchez"}}
+    #return users.val()
+    return body
+    #return 'do some magic!'
 
 
 def delete_user(UserName, body):  # noqa: E501
@@ -49,4 +68,8 @@ def get_all_users(body):  # noqa: E501
     """
     if connexion.request.is_json:
         body = User.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    users = db.child("user").get()
+    print(users.val()) # {"Morty": {"name": "Mortimer 'Morty' Smith"}, "Rick": {"name": "Rick Sanchez"}}
+    return users.val()
+    #return 'do some magic!'
+
