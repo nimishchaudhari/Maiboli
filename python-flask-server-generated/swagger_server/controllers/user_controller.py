4a63a5af545ec8ab03eb4b5fc3dd44f5a12f50ec
Nimish Chaudhari <nimishchaudhari@MacBook-Air.local>
2020-04-14 22:29:24 +0200

    Added auth token, wroks!!

--- python-flask-server-generated/swagger_server/controllers/user_controller.py
+++ python-flask-server-generated/swagger_server/controllers/user_controller.py
@@ -1,10 +1,29 @@
 import connexion
 import six
-
+import pyrebase
 from swagger_server.models.user import User  # noqa: E501
 from swagger_server import util
 
+#test@maiboli.fr
+#123123
 
+firebaseConfig = {
+  "apiKey": "AIzaSyDNtFUjYR4pp09Vx526eDrcX4jjIvq-1XM",
+  "authDomain": "maiboli-rest-api.firebaseapp.com",
+  "databaseURL": "https://maiboli-rest-api.firebaseio.com",
+  "projectId": "maiboli-rest-api",
+  "storageBucket": "maiboli-rest-api.appspot.com",
+  "messagingSenderId": "785641769787",
+  "appId": "1:785641769787:web:1818d34874193d52ee051e",
+  "measurementId": "G-WD78BEMMP3"
+}
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+auth = firebase.auth()
+user = auth.sign_in_with_email_and_password("test@maiboli.fr", '123123')
+db = firebase.database()
+user = auth.refresh(user['refreshToken'])
+token = user['idToken']
 def create_user(body):  # noqa: E501
     """Creates a customer.
 
@@ -17,7 +36,12 @@ def create_user(body):  # noqa: E501
     """
     if connexion.request.is_json:
         body = User.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    db.child("userlist").push(body.id,token)
+    #db.child("user").child(body.id).set(body.id)
+    #db.child("user").child(body.id).set(body._pass)
+    #db.child("user").child(body.id).set(body.)
+#    db.child("users").child("Morty").set(data)
+    return str(body)
 
 
 def delete_user(id):  # noqa: E501
